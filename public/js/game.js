1f5027e66e6129f4eed2d193b00531c97f0c61bb
Daniel Pencak <danielpencak@gmail.com>
2017-01-07 18:29:05 -0800

    Add background and ship

--- public/js/game.js
+++ public/js/game.js
@@ -1,23 +1,33 @@
-window.onload = function() {
+/* eslint-disable*/
+// 'use strict';
 
-    //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-    //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
-    //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
-
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
-
-    function preload () {
-
-        // game.load.image('logo', 'phaser.png');
-        game.load.image('deepSpace', '/assets/deep-space.jpg')
-
-    }
-
-    function create () {
-
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'deepSpace');
-        logo.anchor.setTo(0.5, 0.5);
-
-    }
-
+var gameProperties = {
+   screenWidth: 640,
+   screenHeight: 480,
 };
+
+var shipProperties = {
+   // start coordinates
+   startX: gameProperties.screenWidth * 0.5,
+   startY: gameProperties.screenHeight * 0.5,
+};
+
+const graphicAssets = {
+ background: { URL:'/assets/deep-space.jpg', name: 'deepSpace' },
+ ship: { URL: '/assets/xenon2_ship.png', name: 'ship'}
+}
+
+window.onload = (() => {
+  const preload = (() => {
+    game.load.image(graphicAssets.background.name, graphicAssets.background.URL);
+    game.load.image(graphicAssets.ship.name, graphicAssets.ship.URL);
+  });
+
+  const create = (() => {
+    const deepSpace = game.add.sprite(0, 0, 'deepSpace');
+
+    // deepSpace.anchor.setTo(.5, .5);
+    const ship = game.add.sprite(shipProperties.startX, shipProperties.startY, 'ship');
+  });
+  const game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, '', { preload: preload, create: create });
+});
