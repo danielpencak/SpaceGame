5b330d669e028cfd693286c3fe374a24c05722c6
will-206 <will9_30@yahoo.com>
2017-01-16 19:29:07 -0800

    Close #36 improve leaderboard style

--- public/js/leaderboards.js
+++ public/js/leaderboards.js
@@ -2,3 +2,13 @@ $(document).ready(function(){
   $('.modal').modal();
   $('.scrollspy').scrollSpy();
 });
+$('.container').scroll(function (event) {
+    var scroll = $('.container').scrollTop();
+    console.log(scroll);
+    if (scroll > 0) {
+      $('.leaderboardsTitle').addClass('opaque');
+    }
+    else {
+      $('.leaderboardsTitle').removeClass('opaque');
+    }
+});
