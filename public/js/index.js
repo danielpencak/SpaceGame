b4766285a460bb958298fa108855053da239fd51
will-206 <will9_30@yahoo.com>
2017-01-17 12:23:37 -0800

    Close #43 Make leaderboards table within modal on home page

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
   $('.modal').modal();
 });
+$('.modal-content').scroll(function (event) {
+    var scroll = $('.modal-content').scrollTop();
+    console.log(scroll);
+    if (scroll > 0) {
+      $('.leaderboardsTitle').addClass('opaque');
+    }
+    else {
+      $('.leaderboardsTitle').removeClass('opaque');
+    }
+});
