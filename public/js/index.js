10cdfe668d06528cb028fe2407dea4edc861c504
will-206 <will9_30@yahoo.com>
2017-01-16 18:51:57 -0800

    removed some comments

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,3 @@
 $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
+  $('.modal').modal();
 });
