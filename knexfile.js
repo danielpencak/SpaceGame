3cffde82c39653344c392d4266eae2c722c61ff2
will-206 <will9_30@yahoo.com>
2017-01-19 13:53:10 -0800

    heroku deployment 1 Close# 26

--- knexfile.js
+++ knexfile.js
@@ -4,15 +4,15 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/spaceRace_dev'
-  }
+  },
 
   // test: {
   //   client: 'pg',
   //   connection: 'postgres://localhost/spaceRace_test'
   // },
-  //
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL
-  // }
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
