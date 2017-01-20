8f48e2885216ac4de30a757a96be8ac10f077ea1
will-206 <will9_30@yahoo.com>
2017-01-19 20:47:42 -0800

    fix login validation

--- validations/token.js
+++ validations/token.js
@@ -7,14 +7,11 @@ module.exports.post = {
     username: Joi.string()
       .label('Username')
       .required()
-      .min(3)
-      .max(15)
       .trim(),
 
     password: Joi.string()
       .label('Password')
       .required()
       .trim()
-      .min(8)
   }
 };
